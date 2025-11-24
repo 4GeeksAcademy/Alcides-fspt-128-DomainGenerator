7a652cbafd894b5ecc31dfaf44da55b9b93371b5
Alcides Rivera <elarsi626@gmail.com>
2025-11-24 19:03:51 +0000

    Casi

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let dom = ['.com','.es','.net','.io']
 
+for (let i = 0; i < pronoun.length; i++) {
+  for (let j = 0; j < adj.length; j++) {
+    for (let k = 0; k < noun.length; k++) {
+      for (let m = 0; m < dom.length; m++){
+        console.log(pronoun[i]+adj[j]+noun[k]+dom[m]);
+      }
+    }
+  }
+}
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
